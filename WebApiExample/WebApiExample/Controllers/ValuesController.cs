f037ae265a8929d3df9551db335b07d8732f3e8d
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2019-01-09 12:59:26 +0200

    Keytest

--- WebApiExample/WebApiExample/Controllers/ValuesController.cs
+++ WebApiExample/WebApiExample/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApiExample.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value-null", "value-not-null" };
         }
 
         // GET api/values/5
