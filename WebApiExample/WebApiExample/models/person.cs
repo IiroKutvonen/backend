d7bba6e3c3dc5f17a4f92c287bf2715e20b520d6
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2019-01-09 15:14:20 +0200

    Database testing

---
+++ WebApiExample/WebApiExample/models/person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiExample.models
+{
+    public partial class Person
+    {
+        public 
+            Person(string name, int? age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        public int? Age { get; set; }
+    }
+}
---
+++ WebApiExample/WebApiExample/models/phone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApiExample.models
+{
+    public partial class Phone
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Type { get; set; }
+        public int? Number { get; set; }
+        public int? PersonId { get; set; }
+    }
+}
